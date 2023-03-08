93f683cc6e5d562abf75af78be169b3d4a9c7031
孟子(曲中岭) <quzhongling@corp.netease.com>
2023-03-08 15:23:44 +0800

    兼容末行模式”冒号+数字“的输入形式

--- app/route.py
+++ app/route.py
@@ -232,7 +232,7 @@ def terminal_socket(ws, cluster, namespace, pod, container):
                         ws.close()
                     if cmd_in != '' and message != '\t': # 排除：回车、或者table键
                         print(cmd_in.encode('utf8'))
-                        # cmd_in 举例， /usr/bin/wget 'aa.com/a.txt'、ps ux、ip, :开头是vim编辑模式
+                        # cmd_in 举例， /usr/bin/wget 'aa.com/a.txt'、ps ux、ip, :开头是vim末行模式
                         if not Common.is_allow_command(cmd_in.split(' ')[0].split('/')[-1]) and not cmd_in.startswith(':'):
                             message = '\x03' # 发送ctrl+c
                             ws.send(' -> 不允许执行' + cmd_in.split(' ')[0] + '    ')
