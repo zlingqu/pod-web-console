044d8fafed000e334df543fb325132bfe6ac558f
孟子(曲中岭) <quzhongling@corp.netease.com>
2023-08-14 10:33:20 +0800

    兼容redis有密码的情况

--- app/redis.py
+++ app/redis.py
@@ -7,16 +7,21 @@ import app.config.config as Config
 class RedisResource(object):
     def __init__(self, expire=None, key_prefix='', *args, **kwargs):
         redis_url = Config.REDIS
-        host, port, db = re.match(r'redis://(.*):(.*)/(.*)', redis_url).groups()
+        passwd, host, port, db = re.match(r'redis://(.*)@(.*):(.*)/(.*)', redis_url).groups()
         if not hasattr(RedisResource, 'pool'):
-            RedisResource.getRedisCoon(host, port, db, *args, **kwargs)
+            RedisResource.getRedisCoon(host, port, db, passwd, *args, **kwargs)
         self.client = redis.StrictRedis(connection_pool=RedisResource.pool)
 
     @staticmethod
-    def getRedisCoon(host, port, db, *args, **kwargs):
+    def getRedisCoon(host, port, db, passwd, *args, **kwargs):
+        if passwd == 'None':
+            my_passwd = ''
+        else:
+            my_passwd = passwd
+
         RedisResource.pool = redis.ConnectionPool(host=host, port=port,
-                                                  db=db, socket_connect_timeout=0.5,
-                                                  *args, **kwargs)
+                                    db=db, password=my_passwd, socket_connect_timeout=0.5,
+                                    *args, **kwargs)
     def read(self, key):
         serialize = self.client.get(key)
         if serialize is not None:
